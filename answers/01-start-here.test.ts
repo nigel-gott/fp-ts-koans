a5052a2cbaac7e4b491ba1c08ba947a9692b4819
Nigel Gott <nigel.gott@gmail.com>
2020-05-12 21:05:03 +0100

    Add first koan with answer!

---
+++ answers/01-start-here.test.ts
@@ -0,0 +1,11 @@
+// Unused imports are hints!
+import { flow } from "fp-ts/lib/function";
+
+test("compose your functions with flow", () => {
+  const addFive = (x) => x + 5;
+  const addTen = (x) => x + 10;
+
+  const addFiveAndTen = flow(addFive, addTen) 
+
+  expect(addFiveAndTen(10)).toBe(25);
+});
